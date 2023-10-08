b146d40aa56beba97822fdabaa4015f658e91da5
qsspy <danelfilipek@gmail.com>
2023-10-08 17:51:58 +0200

    Prepared base branch for measurements

---
+++ commons/src/main/java/com/qsspy/commons/port/output/publisher/notification/MeasurementStartedNotificationEvent.java
@@ -0,0 +1,38 @@
+package com.qsspy.commons.port.output.publisher.notification;
+
+import com.qsspy.commons.architecture.eda.NotificationEvent;
+
+import java.util.UUID;
+
+public record MeasurementStartedNotificationEvent(
+        UUID eventId,
+        long occurredOn,
+
+        MeasurementType measurementType,
+        int recordNumber,
+        int description
+) implements NotificationEvent {
+
+    private static final int EVENT_VERSION = 1;
+    private static final String EVENT_TYPE = "measurement.started";
+
+    @Override
+    public String eventType() {
+        return EVENT_TYPE;
+    }
+
+    @Override
+    public int eventVersion() {
+        return EVENT_VERSION;
+    }
+
+    @Override
+    public String destinationBindingName() {
+        return "measurements";
+    }
+
+    @Override
+    public Object partitionKey() {
+        return eventId;
+    }
+}
